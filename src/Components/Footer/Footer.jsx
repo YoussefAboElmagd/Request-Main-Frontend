de31d7732d9e1e62a9ef52dccda2a56d2aa0e90d
Dev-Mohammed <Dev-Mohammed>
2025-02-03 02:45:47 +0200

    feat :add link in the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="flex rounded-t-lg opacity-80 justify-center items-center bg-gray-600 text-white p-4">
       <img src={logo} alt="Logo" className="h-6 mr-2" />
-      <span>Powered By Mdarj</span>
+      <span><a href="https://mdarj.org/" className="hover:underline">Powered By Mdarj</a></span>
     </footer>
   );
 };
